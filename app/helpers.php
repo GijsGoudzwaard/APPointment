31acbe5be0923b2f051d887fbb0f7da49254491d
Gijs Goudzwaard <gijs.goudzwaard@gmail.com>
2015-12-28 22:40:41 +0100

    Added the get_environment helper function

--- app/helpers.php
+++ app/helpers.php
@@ -1,2 +1,9 @@
 <?php
 // Helper functions here
+
+function get_environment() {
+	$subdomain = explode('.', parse_url(url())['host'])[0];
+	$environment = App\Models\Environment::where('subdomain', '=', $subdomain)->get();
+
+	return $environment[0];
+}
