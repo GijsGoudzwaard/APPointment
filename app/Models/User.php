f92b8efb96845a39b2974d30ebad287d9a6d58dd
Gijs Goudzwaard <gijs.goudzwaard@gmail.com>
2016-09-24 14:33:19 +0200

    Updated some other composer packages and fixed a couple of errors

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,22 +2,14 @@
 
 namespace App\Models;
 
-use Illuminate\Auth\Authenticatable;
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
 use Illuminate\Database\Eloquent\SoftDeletes;
-use Illuminate\Auth\Passwords\CanResetPassword;
-use Illuminate\Foundation\Auth\Access\Authorizable;
-use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
-use Illuminate\Contracts\Auth\Access\Authorizable as AuthorizableContract;
-use Illuminate\Contracts\Auth\CanResetPassword as CanResetPasswordContract;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class User extends Model implements
-    AuthenticatableContract,
-use NotifiableAuthorizableContractuseCanResetPasswordContract
+class User extends Authenticatable
 {
     use SoftDeletes;
-
-    use  Authenticatable, Authorizable, CanResetPassword;
+    use Notifiable;
 
     /**
      * The attributes that are mass assignable.
